253dfb67913f648612908bc515e5e86639e5f896
unknown <work.ceative@gmail.com>
2023-05-22 16:50:19 +0500

    hamza rafi

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
+  experimentalStudio: true,
+
   env: {
     Email_Address: 'junaid.amin@yopmail.com',
     Password: '12345678',
   },
   e2e: {
     baseUrl: 'https://career.orcaloholding.co.uk/',
+    excludeSpecPattern: ['*/**/1-getting-started', '*/**/2-advanced-examples'],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,27 +2,40 @@
 
 describe('Authentication Module', () => {
   it('login Positive', () => {
-    cy.visit('/'+'login')
+    cy.visit('/' + 'login')
     cy.login(Cypress.env('Email_Address'), Cypress.env('Password'))
     cy.url().should('eq', Cypress.config('baseUrl'))
-    // cy.contains('Jobs').trigger('mouseup').wait(2000)
-    // cy.contains('My Jobs').click({ force: true })
   })
+
   it('login Negative', () => {
     cy.visit('/' + 'login')
     cy.login(Cypress.env('Email_Address'), '05011997')
-    cy.url().should('eq', Cypress.config('baseUrl')+'login')
+    cy.url().should('eq', Cypress.config('baseUrl') + 'login')
   })
 })
 
-describe.only('Authentication Module', () => {
-  it('Update Profile', () => { 
+describe.only('Update profile', () => {
+  beforeEach(() => {
     cy.visit('/' + 'login')
     cy.login(Cypress.env('Email_Address'), Cypress.env('Password'))
-    cy.get('.btn > .rounded-full').click()
-    if (cy.get('.cursor-pointer > .mt-2').as('element').should('exist')) {
-      cy.get('@element').click().attachFile('cypress/fixtures/img.png')
-    }
+  })
 
+  it('Update Profile', () => {
+    cy.get('.btn > .rounded-full').click()
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/img.png', { force: true })
+    cy.get('div[class="container mx-auto my-32"]').should('contain', 'Profile Picture Changed Successfully')
+    cy.get('#first_name').clear().type('Junaid')
+    cy.get('#last_name').clear().type('Amin')
+    cy.get('#phone_no').clear().type('+9234531568978')
+    cy.get(':nth-child(1) > .my-4 > div.w-full > .border-2').selectFile('cypress/fixtures/res.docx')
+    cy.get(':nth-child(2) > .my-4 > div.w-full > .border-2').selectFile('cypress/fixtures/res.docx')
+    cy.get(':nth-child(5) > .flex > :nth-child(2) > .py-2').click()
+    cy.get('#institute').clear().type('Ucp')
+    cy.get('#degree_title').select("Bachelor's Degree")
+    cy.get('#discipline').select('Anthropology')
+    cy.get('#gpa').clear().type('2.69')
+    cy.get('#document').selectFile('cypress/fixtures/res.docx')
+    cy.contains('Submit').click()
+    cy.get('div[class="container mx-auto my-32"]').should('contain', 'Education detail added successfully')
   })
 })
